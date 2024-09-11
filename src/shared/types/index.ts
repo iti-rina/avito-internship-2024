fd8df1056531312900f02b79b1c969d5d39f70cf
iti-rina <itigecheva.irina@gmail.com>
2024-09-11 11:50:06 +0700

    feat: declare types for project

---
+++ src/shared/types/index.ts
@@ -0,0 +1,48 @@
+export type Advertisment = {
+  id: string;
+  name: string;
+  description?: string;
+  price: number;
+  createdAt: string;
+  views: number;
+  likes: number;
+  imageUrl?: string;
+}
+
+export type Advertisements = {
+  advertisements: Advertisment[]
+}
+
+
+const OrderStatus = {
+  Created: 0,
+  Paid: 1,
+  Transport: 2,
+  DeliveredToThePoint: 3,
+  Received: 4,
+  Archived: 5,
+  Refund: 6
+} as const;
+
+
+export type OrderItem = Advertisment & { count: number; };
+
+export type Order = {
+  id: string;
+  status: typeof OrderStatus[keyof typeof OrderStatus];
+  createdAt: string;
+  finishedAt?: string;
+  items: Array<OrderItem>;
+  deliveryWay: string;
+  total: number;
+}
+
+export type Orders = {
+  orders: Order[]
+}
+
+type Image = {
+  id: number;
+  url: string;
+  name: string;
+}
