837e79f9698dd4106840655b24a35f099c619922
iti-rina <itigecheva.irina@gmail.com>
2024-09-11 19:49:15 +0700

    feat: add ui for advertisements page

--- src/pages/AdvertisementsPage.tsx
+++ src/pages/AdvertisementsPage.tsx
@@ -1,11 +1,32 @@
+import React from "react";
+
+import { AdvertisementsList } from "@features/AdvertisementsList";
+import { Pagination } from "@widgets/pagination";
+import { advertisements } from '../data';
+
+
 interface Props {
 
 }
 
-const AdvertisementPage: React.FC<Props> = () => {
+const AdvertisementsPage: React.FC<Props> = () => {
+  let total = advertisements.length;
+  let defaultPageSize = 10;
+  let defaultCurrent = 1;
+
   return (
-    <h1>Мои объявления</h1>
+    <main>
+      <h1>Мои объявления</h1>
+      <AdvertisementsList advertisements={advertisements}/>
+      <Pagination 
+        total={total}
+        showTotal={(total) => `Total ${total} items`}
+        defaultPageSize={defaultPageSize}
+        defaultCurrent={defaultCurrent}
+      />
+    </main>
+
   );
 }
 
-export default AdvertisementPage;
+export default AdvertisementsPage;
