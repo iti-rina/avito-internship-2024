24bc4ea7a880aecc5a0819df809279dde62b95f0
iti-rina <itigecheva.irina@gmail.com>
2024-09-13 03:42:53 +0700

    fix: remove pagination

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -5,23 +5,12 @@ import { ordersStore } from '@app/store';
 import { FilterComponent } from '@widgets/filter/idex';
 
 const OrdersPage: React.FC = () => {
-  const { orders } = ordersStore;
-  let total = orders.length;
-  let defaultPageSize = 10;
-  let defaultCurrent = 1;
-
   return (
     <>
       <FilterComponent />
       <main>
         <h1>Заказы</h1>
         <OrdersList />
-        <Pagination 
-          total={total}
-          showTotal={(total) => `Всего ${total}`}
-          defaultPageSize={defaultPageSize}
-          defaultCurrent={defaultCurrent}
-        />
       </main>
     </>
 
