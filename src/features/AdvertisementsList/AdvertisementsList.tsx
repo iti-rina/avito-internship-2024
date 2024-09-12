f56d391acbe045438f485ea8da088f3358c846de
iti-rina <itigecheva.irina@gmail.com>
2024-09-12 18:08:54 +0700

    fix: move SerchBar to another pafe

--- src/features/AdvertisementsList/AdvertisementsList.tsx
+++ src/features/AdvertisementsList/AdvertisementsList.tsx
@@ -3,10 +3,11 @@ import { List } from "antd";
 import { AdvertisementCard } from "@features/AdvertisementCard";
 import { observer } from "mobx-react-lite";
 import { advertisementsStore } from "@app/store";
+import SearchBar from "@widgets/header/SearchBar";
 
 const AdvertisementsList: React.FC = observer(() => {
   useEffect(() => {
-    advertisementsStore.getAdvertisements();
+    advertisementsStore.getAdvertisements(advertisementsStore.currentPage);
   }, []);
 
   if (advertisementsStore.loading) {
@@ -18,20 +19,23 @@ const AdvertisementsList: React.FC = observer(() => {
   }
   
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      dataSource={advertisementsStore.advertisements}
-      renderItem={item => (
-        <AdvertisementCard
+    <main>
+      <SearchBar />
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={advertisementsStore.advertisements}
+        renderItem={item => (
+          <AdvertisementCard
           name={item.name}
           imageUrl={item.imageUrl ? item.imageUrl : ''}
           price={item.price}
           views={item.views}
           likes={item.likes}
-        />
-      )}
-    />
+          />
+        )}
+      />
+    </main>
   );
 });
 
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Navigation from './Navigation'
-import SearchBar from './SearchBar';
-import { Space } from 'antd';
 
 const Header: React.FC = () => {
   return (
     <header>
         <Navigation />
-        <SearchBar />
     </header>
   );
 }
