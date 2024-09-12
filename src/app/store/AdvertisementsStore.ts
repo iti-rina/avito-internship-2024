b88f61f4ba9713d2532694c15ebfb6702ceb37ee
iti-rina <itigecheva.irina@gmail.com>
2024-09-12 18:09:56 +0700

    feat: add pagination to a list of advertisements

--- src/app/store/AdvertisementsStore.ts
+++ src/app/store/AdvertisementsStore.ts
@@ -3,27 +3,34 @@ import { AdvertisementType, AdvertisementToSendType } from '@shared/types';
 
 class AdvertisementsStore {
   advertisements: AdvertisementType[] = [];
+  currentPage = 1;
+  elementsPerPage = 10;
   loading: boolean = false;
   error: string | null = null;
+  hasMore = true;
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  async getAdvertisements() {
+  async getAdvertisements(page: number) {
     this.loading = true;
     this.error = null;
 
     try {
-      const response = await fetch('http://localhost:3000/advertisements');
+      const response = await fetch(`http://localhost:3000/advertisements?_start=${page === 1 ? 0 : (this.currentPage - 1) * this.elementsPerPage }&_limit=${this.elementsPerPage}`);
       if (!response.ok) {
         throw new Error('Error while getting a list of advertisements');
       }
       const data = await response.json();
 
       runInAction(() => {
-        this.advertisements = data;
+        this.advertisements = [...this.advertisements, ...data];
+        this.currentPage = page;
         this.loading = false;
+        if (data.length < this.elementsPerPage) {
+          this.hasMore = false;
+        }
       });
     } catch (error: any) {
       runInAction(() => {
@@ -46,12 +53,20 @@ class AdvertisementsStore {
         throw new Error('Error while sending a new advertisement to the server');
       }
       const data = await response.json();
-
-      runInAction(() => {
-        this.advertisements.push(data);
-      });
+      this.getAdvertisements(this.currentPage);
     } catch {
     }
+  }
+
+  setPage(page: number) {
+    if (!this.hasMore || this.loading) return;
+    this.currentPage = page;
+    this.getAdvertisements(page);
+  }
+
+  setPageSize(pageSize: number) {
+    this.elementsPerPage = pageSize;
+    this.getAdvertisements(this.currentPage);
   }
 }
 
--- src/widgets/pagination/Pagination.tsx
+++ src/widgets/pagination/Pagination.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Pagination, PaginationProps } from 'antd';
+import { observer } from 'mobx-react-lite';
+import { advertisementsStore } from '@app/store';
 
-const PaginationComponent: React.FC<PaginationProps> = ({total, showTotal, defaultPageSize, defaultCurrent}) => (
-  <>
+const PaginationComponent: React.FC<PaginationProps> = observer(({showTotal}) => {
+  const handlePageChange = (page: number) => {
+    advertisementsStore.setPage(page);
+  }
+  const handleChangePageSize = (current: number, size: number) => {
+    advertisementsStore.setPageSize(size);
+  }
+  return (
     <Pagination
-      total={total}
-      showTotal={showTotal}
-      defaultPageSize={defaultPageSize}
-      defaultCurrent={defaultCurrent}
+      current={advertisementsStore.currentPage}
+      pageSize={advertisementsStore.elementsPerPage}
+      onChange={(page) => handlePageChange(page)}
+      total={advertisementsStore.hasMore ? advertisementsStore.currentPage * advertisementsStore.elementsPerPage : advertisementsStore.advertisements.length}
       showSizeChanger
+      onShowSizeChange={(current, size) => handleChangePageSize(current, size)}
       locale={{
         items_per_page: 'на странице'
       }}
     />
-  </>
-);
+  );
+
+});
 
 export default PaginationComponent;
