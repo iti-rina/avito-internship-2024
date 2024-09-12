23c4c1d2c8b977186f249a1b32b224d0f8b36120
iti-rina <itigecheva.irina@gmail.com>
2024-09-13 03:51:52 +0700

    feat: add some styles

--- src/features/AdvertisementCard/AdvertisementCard.tsx
+++ src/features/AdvertisementCard/AdvertisementCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { List } from 'antd';
+import { useLocation } from 'react-router-dom';
+import { Divider, List, Space, Typography } from 'antd';
 import { EyeOutlined, LikeOutlined } from '@ant-design/icons';
 import { Icon } from '@shared/components';
 import { Link } from 'react-router-dom';
@@ -14,23 +15,28 @@ interface AdvertisementProps {
 }
 
 const AdvertisementCard: React.FC<AdvertisementProps> = ({ id, name, imageUrl, price, views, likes }) => {
+  let { pathname: currentPage} = useLocation();
+
   return (
+    <>
     <Link to={`/advertisements/${id}`}>
       <List.Item
       >
-        <List.Item.Meta 
-          title={name}
-          />
-        <img 
+        <Typography.Title level={2}>{name}</Typography.Title>
+          <img 
             src={imageUrl}
             alt={`Изображение ${name}`}
-            width='272'
-            />
-        <p>Стоимость: {price}</p>
-        <Icon icon={LikeOutlined} textForIcon={String(likes)} />
-        <Icon icon={EyeOutlined} textForIcon={String(views)} />
+            width={currentPage === '/orders' ? 150 : 272}
+          />
+          <p>Стоимость: {price}</p>
+          <Space>
+            <Icon icon={LikeOutlined} textForIcon={String(likes)} />
+            <Icon icon={EyeOutlined} textForIcon={String(views)} />
+          </Space>
       </List.Item>
     </Link>
+    <Divider />
+    </>
   );
 }
 
