6817fad87036635d25fe4b2479ee486ce7200962
iti-rina <itigecheva.irina@gmail.com>
2024-09-12 22:33:28 +0700

    feat: add the main logic to search ads by name

--- src/widgets/header/SearchBar.tsx
+++ src/widgets/header/SearchBar.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Input, GetProps } from 'antd';
+import { searchAdvertisementByName } from '@entities/advertisement/api';
+import { observer } from 'mobx-react-lite';
+import { advertisementsStore } from '@app/store';
 
 const { Search } = Input;
 type SearchProps = GetProps<typeof Input.Search>;
 
-const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+const onSearch: SearchProps['onSearch'] = (value, _e, info) => {
+  let searchQuery = value.trim().toLowerCase();
+  advertisementsStore.searchAvertisementsByName(searchQuery);
+};
 
-const SearchBar: React.FC = () => {
+const SearchBar: React.FC = observer(() => {
   return (
     <Search placeholder='Поиск по объявлениям' onSearch={onSearch} enterButton />
   );
-}
+})
 
 export default SearchBar;
